47e0636c0c19b6b248404deab740c54960455210
Nicholas Ochs <nick.ochs@yahoo.com>
2021-08-28 19:12:13 -0400

    initial commit & setup

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from '@material-ui/core';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#FFC0CB'
+        }
+    }
+});
+
+export default theme;
