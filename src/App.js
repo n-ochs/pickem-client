5a36b38bcd6faafde694f1bf3587d31e3f9c0656
Nicholas Ochs <nick.ochs@yahoo.com>
2021-08-28 23:00:46 -0400

    adds navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import theme from './theme';
 import { ThemeProvider } from '@material-ui/core';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Router>
-
+      <Navbar />
       </Router>
     </ThemeProvider>
   );
